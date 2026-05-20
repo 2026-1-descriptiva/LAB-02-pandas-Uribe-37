d1c6ee064f54a490c8ca036a8a839277f800a153
Uribe-37 <juribeca@unal.edu.co>
2026-05-20 17:14:00 -0500

    Prueba

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -7,7 +7,7 @@ librerias de pandas para resolver las preguntas.
 import pandas as pd
 
 def pregunta_01():
-    df = pd.read_table(r"files\input\tbl0.tsv", sep="\t")
+    df = pd.read_csv(r"files\input\tbl0.tsv", sep="\t")
     return df.shape[0]
 
     """
